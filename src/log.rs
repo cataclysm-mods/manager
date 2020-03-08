d92a1371e91000ad81aa774ca0eed7e5f481a3aa
Damien Wilson <damien@mindglob.com>
2020-03-08 13:45:46 -0700

    Initial commit

---
+++ src/log.rs
@@ -0,0 +1,19 @@
+use tracing::subscriber::SetGlobalDefaultError;
+use tracing_subscriber::{fmt, registry::Registry, EnvFilter};
+use tracing_subscriber::prelude::*;
+
+pub fn init() -> Result<(), SetGlobalDefaultError> {
+    let fmt_layer = fmt::Layer::builder()
+        .with_target(true)
+        .finish();
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    let subscriber = Registry::default()
+        .with(filter_layer)
+        .with(fmt_layer);
+
+    tracing::subscriber::set_global_default(subscriber)
+}
